cf00bf4a3588105503355083fd0f84cf9b03d206
obutora <obutora@gmail.com>
2022-06-03 17:41:47 +0900

    add : get safeCoff from index.html

--- python/app.py
+++ python/app.py
@@ -38,36 +38,41 @@ def upload_file():
         os.mkdir('upload/')
         os.mkdir('output/')
 
-        try:
-            file1 = request.files['file1']
-            file2 = request.files['file2']
-            file3 = request.files['file3']
+        # 安全係数
+        safeCoff = request.form.get('week')
 
-            file1.save(os.path.join('./upload/', file1.filename))
-            file2.save(os.path.join('./upload/', file2.filename))
-            file3.save(os.path.join('./upload/', file3.filename))
+        return safeCoff
 
-            order_to_chois.order_to_chois(
-                os.path.join('./upload/', file1.filename),
-                os.path.join('./upload/', file2.filename),
-                os.path.join('./upload/', file3.filename)
-            )
+        # try:
+        #     file1 = request.files['file1']
+        #     file2 = request.files['file2']
+        #     file3 = request.files['file3']
 
-            with zipfile.ZipFile('./output/result.zip', 'w', compression=zipfile.ZIP_DEFLATED, compresslevel=9) as zf:
-                zf.write('./output/check.csv',
-                         arcname='【要確認】直近で発注歴がないJAN、発注する場合は手動でお願いします.csv')
-                zf.write('./output/detail.csv',
-                         arcname='【必要に応じて確認】発注予定リスト.csv')
-                zf.write('./output/order.csv', arcname='【CHOIS用】一括発注用ファイル.csv')
+        #     file1.save(os.path.join('./upload/', file1.filename))
+        #     file2.save(os.path.join('./upload/', file2.filename))
+        #     file3.save(os.path.join('./upload/', file3.filename))
 
-            # return send_file(
-            #     './output/result.zip',
-            #     as_attachment=True
-            # )
-            return render_template('download.html')
+        #     order_to_chois.order_to_chois(
+        #         os.path.join('./upload/', file1.filename),
+        #         os.path.join('./upload/', file2.filename),
+        #         os.path.join('./upload/', file3.filename)
+        #     )
 
-        except:
-            return render_template('error.html')
+        #     with zipfile.ZipFile('./output/result.zip', 'w', compression=zipfile.ZIP_DEFLATED, compresslevel=9) as zf:
+        #         zf.write('./output/check.csv',
+        #                  arcname='【要確認】直近で発注歴がないJAN、発注する場合は手動でお願いします.csv')
+        #         zf.write('./output/detail.csv',
+        #                  arcname='【必要に応じて確認】発注予定リスト.csv')
+        #         zf.write('./output/order.csv', arcname='【CHOIS用】一括発注用ファイル.csv')
+
+        #     # return send_file(
+        #     #     './output/result.zip',
+        #     #     as_attachment=True
+        #     # )
+        #     return render_template('download.html')
+
+        # except:
+        #     return render_template('error.html')
     else:
         return redirect('/')
 
