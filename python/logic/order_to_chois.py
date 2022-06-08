3309009879b6e3fcc9c82f3f7eb4adf5a0cce125
chan haga <obutora@gmail.com>
2022-06-09 01:30:08 +0900

    fix: improve pandas performance

--- python/logic/order_to_chois.py
+++ python/logic/order_to_chois.py
@@ -3,15 +3,16 @@
 
 import pandas as pd
 import numpy as np
-import warnings
-warnings.simplefilter('ignore', FutureWarning)
+import re
 
 
 def order_to_chois(in1y, out6m, iyakusyu):
     # %%
     # inDF = pd.read_csv('csv/in_past1y.csv', encoding='cp932')
-    inDF = pd.read_csv(in1y, encoding='cp932')
-    inDF = inDF.loc[:, ['商品コード', '医薬品名', 'バラ換算数量', '箱数量', '仕入単価']]
+    inUseCols = ['商品コード', '医薬品名', 'バラ換算数量', '箱数量', '仕入単価']
+    inDF = pd.read_csv(in1y, encoding='cp932', usecols=inUseCols)
+
+    # inDF = inDF.loc[:, ['商品コード', '医薬品名', 'バラ換算数量', '箱数量', '仕入単価']]
     inDF = inDF.sort_index(ascending=False)
     inDF = inDF.drop_duplicates()
     inDF['包装単位'] = inDF['バラ換算数量'] / inDF['箱数量']
@@ -28,14 +29,26 @@ def order_to_chois(in1y, out6m, iyakusyu):
     # 出庫量の読み込みと、必要な列のみ抽出
     # outDF = pd.read_csv(
     #     'csv/2021_12_2022_04_out_medicine.csv', encoding='cp932')
-    outDF = pd.read_csv(out6m, encoding='cp932')
+    outUseCols = ['医薬品名', '伝票日付', 'バラ換算数量', '規制', '薬価単位']
+    outDF = pd.read_csv(out6m, encoding='cp932', usecols=outUseCols, dtype={
+        '医薬品名': str,
+        '伝票日付': str,
+        'バラ換算数量': float,
+        '規制': 'category',
+        '薬価単位': 'category'}
+    )
+
     outDF = outDF[outDF['規制'] != '麻薬']
     outDF = outDF[outDF['薬価単位'] != 'ＭＬ']
-    outDF = outDF.loc[:, ['医薬品名', '伝票日付', 'バラ換算数量']]
+    # outDF = outDF.loc[:, ['医薬品名', '伝票日付', 'バラ換算数量']]
+    outDF.drop(['規制', '薬価単位'], axis='columns', inplace=True)
 
     # 日付を年月のみにしたいのでDateTimeにしてからStringにformatしなおしている
-    outDF['伝票日付'] = pd.to_datetime(outDF['伝票日付'])
-    outDF['伝票日付'] = outDF['伝票日付'].apply(lambda x: x.strftime('%Y-%m'))
+    # outDF['伝票日付'] = pd.to_datetime(outDF['伝票日付'])
+    # outDF['伝票日付'] = outDF['伝票日付'].apply(lambda x: x.strftime('%Y-%m'))
+    m = re.compile(r'\d{4}\/([1-9])\/(0?[1-9]|[12][0-9]|3[01])$')
+    outDF['伝票日付'] = outDF['伝票日付'].apply(
+        lambda x: x[:6] if m.match(x) else x[:7])
     # outDF.head()
     # print(len(outDF))
 
@@ -51,9 +64,11 @@ def order_to_chois(in1y, out6m, iyakusyu):
     outPivot['avg'] = avg
     # outPivot.head()
 
-    # %%
-    medDF = pd.read_csv(iyakusyu, encoding='cp932')
-    medDF = medDF.loc[:, ['医薬品名', 'YJコード', 'JANコード', '在庫数量']]
+    medUseCols = ['医薬品名', 'YJコード', 'JANコード', '在庫数量']
+    medDF = pd.read_csv(iyakusyu, encoding='cp932', usecols=medUseCols, dtype={
+        '医薬品名': str, 'YJコード': str, 'JANコード': str, '在庫数量': float
+    })
+    # medDF = medDF.loc[:, ['医薬品名', 'YJコード', 'JANコード', '在庫数量']]
     # medDF = medDF.loc[:, ['医薬品名','在庫数量']]
 
     # medDF.head()
@@ -64,10 +79,10 @@ def order_to_chois(in1y, out6m, iyakusyu):
     # joinDF = outPivot.merge(inDF, how='left', on='医薬品名')
 
     joinDF.rename(columns={'JANコード_x': 'JANコード'}, inplace=True)
-    joinDF.drop('JANコード_y', axis='columns', inplace=True)
+    # joinDF.drop('JANコード_y', axis='columns', inplace=True)
     joinDF = joinDF.drop_duplicates(subset=['医薬品名'])
 
-    needCheckDF = joinDF[joinDF['包装単位'].isnull()]
+    # needCheckDF = joinDF[joinDF['包装単位'].isnull()]
     # needCheckDF.to_csv(
     #     'output/check.csv', encoding='cp932')
 
@@ -102,14 +117,14 @@ def order_to_chois(in1y, out6m, iyakusyu):
     # joinDF.to_csv('output/detail.csv', encoding='cp932')
 
     # %%
-    orderDF = joinDF.loc[:, ['JANコード', '発注数', '医薬品名']]
-    orderDF.rename(columns={'医薬品名': '販売名称'}, inplace=True)
-    orderDF['規格'] = 0
-    orderDF['総入数／単位'] = 0
-    orderDF['MC送信卸コード'] = 0
+    # orderDF = joinDF.loc[:, ['JANコード', '発注数', '医薬品名']]
+    # orderDF.rename(columns={'医薬品名': '販売名称'}, inplace=True)
+    # orderDF['規格'] = 0
+    # orderDF['総入数／単位'] = 0
+    # orderDF['MC送信卸コード'] = 0
 
-    orderDF['JANコード'] = orderDF['JANコード'].astype(str).str[:-2]
-    orderDF['発注数'] = orderDF['発注数'].astype(str).str[:-2]
+    # orderDF['JANコード'] = orderDF['JANコード'].astype(str).str[:-2]
+    # orderDF['発注数'] = orderDF['発注数'].astype(str).str[:-2]
 
     # orderDF.head()
 
